9526fc96f975cb4d3012d756af6540c8727c703a
jehough <teachlife@outlook.com>
2019-06-06 00:01:02 -0400

    fixes authorization issues

--- lib/baseballcliapiversion/api.rb
+++ lib/baseballcliapiversion/api.rb
@@ -1,10 +1,18 @@
 class Baseballcliapiversion::Api
-    def self.get_games
-      d = Date.today.prev_day
-      date = d.strftime('%Y%m%d')
-      resp = HTTParty.get("https://api.mysportsfeeds.com/v1.2/pull/mlb/current/daily_game_schedule.json?fordate=#{date}", {
-          header: {Authorization: Basic {"#{ENV[MySportsFeedCredentials]}"}}
-    })
-    resp
-    end
+    def self.send_request
+        uri = URI("https://api.mysportsfeeds.com/v1.2/pull/mlb/2019-regular/daily_game_schedule.json?fordate=20190406")
+      
+        # Create client
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      
+        # Create Request
+        req =  Net::HTTP::Get.new(uri)
+        # Add headers
+        req.basic_auth("#{ENV[Sportsfeed_key]}", "#{ENV[Sportsfeed_pw]}")
+        # Fetch Request
+        res = http.request(req).body
+      end
+    
 end
