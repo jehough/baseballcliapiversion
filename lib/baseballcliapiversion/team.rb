d4466f631785454c1fa1e96073fc35441eb15f23
jehough <teachlife@outlook.com>
2019-06-07 09:26:09 -0400

    adds attributes to team

--- lib/baseballcliapiversion/team.rb
+++ lib/baseballcliapiversion/team.rb
@@ -1,5 +1,15 @@
 
 class Baseballcliapiversion::Team
     attr_accessor :name, :game_id, :hits, :doubles, :triples, :homers, :rbis, :steals, :team_avg, :team_ops, :pitchks, :teamera, :teampip, :innings
-
+    
+    def self.create_from_hash(hash)
+        team = self.new
+        team.attrs_from_hash(hash)
+    end    
+    
+    def attrs_from_hash(hash)
+        hash.each do |k,v|
+            send ("#{k}=", v)
+        end
+    end
 end
