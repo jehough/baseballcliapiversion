22c2bd1621218b31b4948b0c4590c660a684de33
jehough <teachlife@outlook.com>
2019-06-06 23:33:24 -0400

    builds player creation

--- lib/baseballcliapiversion/player.rb
+++ lib/baseballcliapiversion/player.rb
@@ -1,4 +1,17 @@
 class Baseballcliapiversion::Player
-    attr_accessor :name
+    attr_accessor :name, 
     def initialize
+    end
+
+    def self.create_from_hash(player_hash)
+        player = Player.new
+        player.attrs_from_hash(player_hash)
+    end
+
+    def attrs_from_hash(player_hash)
+        player_hash.each do |k , v|
+            send ("#{k}=",v)
+        end
+    end
+
 end
