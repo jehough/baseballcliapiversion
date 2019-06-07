1bd5bdf47eb85d5a741b30432d293fcf6739749e
jehough <teachlife@outlook.com>
2019-06-06 23:20:37 -0400

    starts game class

--- lib/baseballcliapiversion/game.rb
+++ lib/baseballcliapiversion/game.rb
@@ -1,7 +1,23 @@
 class Baseballcliapiversion::Game
     attr_accessor :game_id, :home_team, :away_team
+    @@all
     def initialize (game_id)
         @game_id = game_id
     end
 
+    def self.create_by_array(id_array)
+        id_array.each do |game_id|
+            game = Game.new(game_id)
+            game.save
+        end
+    end
+    
+    def save
+        self.class.all << self
+    end
+    
+    def self.all
+        @@all
+    end
+        
 end
